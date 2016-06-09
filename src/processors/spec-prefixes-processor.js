c8caa10a5483ce133de8cd9fdbcaadb7ce7f35af
Souvik Basu <souvikbasu@gmail.com>
2016-06-09 14:20:49 +0530

    Added support for windows platform to show tick mark and cross symbols

--- src/processors/spec-prefixes-processor.js
+++ src/processors/spec-prefixes-processor.js
@@ -1,11 +1,21 @@
 var DisplayProcessor = require('../display-processor');
 
 function SpecPrefixesProcessor(prefixes) {
+  var successSymbol = '✓ ',
+      failureSymbol = '✗ ',
+      pendingSymbol = '* ';
+
+  if (process && process.platform === 'win32') {
+    successSymbol = '\u221A ';
+    failureSymbol = '\u00D7 ';
+    pendingSymbol = '* ';
+  }
+
   this.prefixes = {
-    success: prefixes && prefixes.success !== undefined ? prefixes.success : '✓ ',
-    failure: prefixes && prefixes.failure !== undefined ? prefixes.failure : '✗ ',
-    pending: prefixes && prefixes.pending !== undefined ? prefixes.pending : '* '
-  };
+    success: prefixes && prefixes.success !== undefined ? prefixes.success : successSymbol,
+    failure: prefixes && prefixes.failure !== undefined ? prefixes.failure : failureSymbol,
+    pending: prefixes && prefixes.pending !== undefined ? prefixes.pending : pendingSymbol
+  }
 }
 
 SpecPrefixesProcessor.prototype = new DisplayProcessor();
