ce35f6ff9f704e430b9c4ef0f92f756b458fd2f1
Bastien Caudan <bastien.caudan@gmail.com>
2016-09-04 19:12:25 +0200

    Use class for metrics and spec reporter

--- src/jasmine-spec-reporter.ts
+++ src/jasmine-spec-reporter.ts
@@ -1,4 +1,6 @@
 import {SpecDisplay} from "./spec-display";
+import {SpecMetrics} from "./spec-metrics";
+
 import {DefaultProcessor} from './processors/default-processor';
 import {SpecColorsProcessor} from './processors/spec-colors-processor';
 import {SpecDurationsProcessor} from './processors/spec-durations-processor';
@@ -7,115 +9,102 @@ import {SuiteNumberingProcessor} from './processors/suite-numbering-processor';
 
 var colors = require('colors');
 
-var SpecMetrics = require('./spec-metrics');
+module.exports = class SpecReporter {
+    private started = false;
+    private finished = false;
+    private options;
+    private display;
+    private metrics;
 
-var SpecReporter = function (options) {
-  this.started = false;
-  this.finished = false;
-  this.options = options || {};
-  initColors(this.options);
-  var displayProcessors = initProcessors(this.options);
-  this.options.hasCustomDisplaySpecStarted = hasCustomDisplaySpecStarted(displayProcessors);
+    constructor(options) {
+        this.options = options || {};
+        SpecReporter.initColors(this.options);
+        var displayProcessors = SpecReporter.initProcessors(this.options);
+        this.options.hasCustomDisplaySpecStarted = SpecReporter.hasCustomDisplaySpecStarted(displayProcessors);
 
-  this.display = new SpecDisplay(this.options, displayProcessors);
-  this.metrics = new SpecMetrics();
-};
+        this.display = new SpecDisplay(this.options, displayProcessors);
+        this.metrics = new SpecMetrics();
+    };
 
-function initColors(options) {
-  colors.setTheme({
-    success: options.colors && options.colors.success ? options.colors.success : 'green',
-    failure: options.colors && options.colors.failure ? options.colors.failure : 'red',
-    pending: options.colors && options.colors.pending ? options.colors.pending : 'yellow'
-  });
-  colors.enabled = true;
-}
-
-function initProcessors(options) {
-  var displayProcessors = [
-    new DefaultProcessor(),
-    new SpecPrefixesProcessor(options.prefixes),
-    new SpecColorsProcessor()
-  ];
-
-  if (options.displaySpecDuration) {
-    displayProcessors.push(new SpecDurationsProcessor());
-  }
-
-  if (options.displaySuiteNumber) {
-    displayProcessors.push(new SuiteNumberingProcessor());
-  }
-
-  if (options.customProcessors) {
-    options.customProcessors.forEach(function (Processor) {
-      displayProcessors.push(new Processor(options));
-    });
-  }
-
-  return displayProcessors;
-}
-
-function hasCustomDisplaySpecStarted(processors) {
-  var isDisplayed = false;
-  processors.forEach(function (processor) {
-    var log = 'foo';
-    var result = processor.displaySpecStarted({id: 'bar', description: 'bar', fullName: 'bar'}, log);
-    isDisplayed = isDisplayed || result !== log;
-  });
-  return isDisplayed;
-}
-
-SpecReporter.prototype = {
-  jasmineStarted: function (info) {
-    this.started = true;
-    this.metrics.start(info);
-    this.display.jasmineStarted(info);
-  },
-
-  jasmineDone: function (info) {
-    this.metrics.stop(info);
-    this.display.summary(this.metrics);
-    this.finished = true;
-  },
-
-  suiteStarted: function (suite) {
-    this.display.suiteStarted(suite);
-  },
-
-  suiteDone: function (suite) {
-    this.display.suiteDone(suite);
-  },
-
-  specStarted: function (spec) {
-    this.metrics.startSpec();
-    this.display.specStarted(spec);
-  },
-
-  specDone: function (spec) {
-    this.metrics.stopSpec(spec);
-    if (spec.status === 'pending') {
-      this.metrics.pendingSpecs++;
-      this.display.pending(spec);
-    } else if (spec.status === 'passed') {
-      this.metrics.successfulSpecs++;
-      this.display.successful(spec);
-    } else if (spec.status === 'failed') {
-      this.metrics.failedSpecs++;
-      this.display.failed(spec);
+    private static initColors(options) {
+        colors.setTheme({
+            success: options.colors && options.colors.success ? options.colors.success : 'green',
+            failure: options.colors && options.colors.failure ? options.colors.failure : 'red',
+            pending: options.colors && options.colors.pending ? options.colors.pending : 'yellow'
+        });
+        colors.enabled = true;
     }
-  },
 
-  reportRunnerStarting: function () {
-      this.display.newLine();
-      this.display.log('*******************************************************************');
-      this.display.log('* Oops!                                                           *');
-      this.display.log('* jasmine-spec-reporter 2.x is not compatible with jasmine < 2.x. *');
-      this.display.log('*                                                                 *');
-      this.display.log('* Please consider using jasmine-spec-reporter < 2.0.0.            *');
-      this.display.log('*                                                                 *');
-      this.display.log('*      npm install jasmine-spec-reporter@"<2.0.0" --save-dev      *');
-      this.display.log('*******************************************************************');
-      this.display.newLine();
-  }
+    private static initProcessors(options) {
+        var displayProcessors = [
+            new DefaultProcessor(),
+            new SpecPrefixesProcessor(options.prefixes),
+            new SpecColorsProcessor()
+        ];
+
+        if (options.displaySpecDuration) {
+            displayProcessors.push(new SpecDurationsProcessor());
+        }
+
+        if (options.displaySuiteNumber) {
+            displayProcessors.push(new SuiteNumberingProcessor());
+        }
+
+        if (options.customProcessors) {
+            options.customProcessors.forEach(function (Processor) {
+                displayProcessors.push(new Processor(options));
+            });
+        }
+
+        return displayProcessors;
+    }
+
+    private static hasCustomDisplaySpecStarted(processors) {
+        var isDisplayed = false;
+        processors.forEach(function (processor) {
+            var log = 'foo';
+            var result = processor.displaySpecStarted({id: 'bar', description: 'bar', fullName: 'bar'}, log);
+            isDisplayed = isDisplayed || result !== log;
+        });
+        return isDisplayed;
+    }
+
+    jasmineStarted(info) {
+        this.started = true;
+        this.metrics.start(info);
+        this.display.jasmineStarted(info);
+    }
+
+    jasmineDone(info) {
+        this.metrics.stop(info);
+        this.display.summary(this.metrics);
+        this.finished = true;
+    }
+
+    suiteStarted(suite) {
+        this.display.suiteStarted(suite);
+    }
+
+    suiteDone(suite) {
+        this.display.suiteDone(suite);
+    }
+
+    specStarted(spec) {
+        this.metrics.startSpec();
+        this.display.specStarted(spec);
+    }
+
+    specDone(spec) {
+        this.metrics.stopSpec(spec);
+        if (spec.status === 'pending') {
+            this.metrics.pendingSpecs++;
+            this.display.pending(spec);
+        } else if (spec.status === 'passed') {
+            this.metrics.successfulSpecs++;
+            this.display.successful(spec);
+        } else if (spec.status === 'failed') {
+            this.metrics.failedSpecs++;
+            this.display.failed(spec);
+        }
+    }
 };
-
-module.exports = SpecReporter;
--- src/spec-metrics.ts
+++ src/spec-metrics.ts
@@ -1,71 +1,68 @@
-var SpecMetrics = function () {
-  this.startTime = null;
-  this.specStartTime = null;
-  this.duration = null;
-  this.successfulSpecs = 0;
-  this.failedSpecs = 0;
-  this.pendingSpecs = 0;
-  this.executedSpecs = 0;
-  this.skippedSpecs = 0;
-  this.totalSpecs = 0;
-  this.totalSpecsDefined = 0;
-  this.random = false;
-  this.seed = null;
-};
+export class SpecMetrics {
+    private startTime = null;
+    private specStartTime = null;
+    private duration = null;
+    private executedSpecs = 0;
+    private skippedSpecs = 0;
+    private totalSpecs = 0;
+    private totalSpecsDefined = 0;
+    private random = false;
+    private seed = null;
 
-SpecMetrics.prototype = {
-  start: function (info) {
-    this.startTime = (new Date()).getTime();
-    this.totalSpecsDefined = info && info.totalSpecsDefined ? info.totalSpecsDefined : 0;
-  },
+    successfulSpecs = 0;
+    failedSpecs = 0;
+    pendingSpecs = 0;
 
-  stop: function (info) {
-    this.duration = this.formatDuration((new Date()).getTime() - this.startTime);
-    this.totalSpecs = this.failedSpecs + this.successfulSpecs + this.pendingSpecs;
-    this.executedSpecs = this.failedSpecs + this.successfulSpecs;
-    this.totalSpecsDefined = this.totalSpecsDefined ? this.totalSpecsDefined : this.totalSpecs;
-    this.skippedSpecs = this.totalSpecsDefined - this.totalSpecs;
-    this.random = info && info.order && info.order.random;
-    this.seed = info && info.order && info.order.seed;
-  },
-
-  startSpec: function () {
-    this.specStartTime = (new Date()).getTime();
-  },
-
-  stopSpec: function (spec) {
-    spec.duration = this.formatDuration((new Date()).getTime() - this.specStartTime);
-  },
-
-  formatDuration: function (durationInMs) {
-    var duration = '', durationInSecs, durationInMins, durationInHrs;
-    durationInSecs = durationInMs / 1000;
-    if (durationInSecs < 1) {
-      return durationInSecs + ' sec' + pluralize(durationInSecs);
+    start(info) {
+        this.startTime = (new Date()).getTime();
+        this.totalSpecsDefined = info && info.totalSpecsDefined ? info.totalSpecsDefined : 0;
     }
-    durationInSecs = Math.round(durationInSecs);
-    if (durationInSecs < 60) {
-      return durationInSecs + ' sec' + pluralize(durationInSecs);
-    }
-    durationInMins = Math.floor(durationInSecs / 60);
-    durationInSecs = durationInSecs % 60;
-    if (durationInSecs) {
-      duration = ' ' + durationInSecs + ' sec' + pluralize(durationInSecs);
-    }
-    if (durationInMins < 60) {
-      return durationInMins + ' min' + pluralize(durationInMins) + duration;
-    }
-    durationInHrs = Math.floor(durationInMins / 60);
-    durationInMins = durationInMins % 60;
-    if (durationInMins) {
-      duration = ' ' + durationInMins + ' min' + pluralize(durationInMins) + duration;
-    }
-    return durationInHrs + ' hour' + pluralize(durationInHrs) + duration;
-  }
-};
 
-function pluralize(count){
-  return count > 1 ? 's' : '';
+    stop(info) {
+        this.duration = this.formatDuration((new Date()).getTime() - this.startTime);
+        this.totalSpecs = this.failedSpecs + this.successfulSpecs + this.pendingSpecs;
+        this.executedSpecs = this.failedSpecs + this.successfulSpecs;
+        this.totalSpecsDefined = this.totalSpecsDefined ? this.totalSpecsDefined : this.totalSpecs;
+        this.skippedSpecs = this.totalSpecsDefined - this.totalSpecs;
+        this.random = info && info.order && info.order.random;
+        this.seed = info && info.order && info.order.seed;
+    }
+
+    startSpec() {
+        this.specStartTime = (new Date()).getTime();
+    }
+
+    stopSpec(spec) {
+        spec.duration = this.formatDuration((new Date()).getTime() - this.specStartTime);
+    }
+
+    formatDuration(durationInMs) {
+        var duration = '', durationInSecs, durationInMins, durationInHrs;
+        durationInSecs = durationInMs / 1000;
+        if (durationInSecs < 1) {
+            return durationInSecs + ' sec' + SpecMetrics.pluralize(durationInSecs);
+        }
+        durationInSecs = Math.round(durationInSecs);
+        if (durationInSecs < 60) {
+            return durationInSecs + ' sec' + SpecMetrics.pluralize(durationInSecs);
+        }
+        durationInMins = Math.floor(durationInSecs / 60);
+        durationInSecs = durationInSecs % 60;
+        if (durationInSecs) {
+            duration = ' ' + durationInSecs + ' sec' + SpecMetrics.pluralize(durationInSecs);
+        }
+        if (durationInMins < 60) {
+            return durationInMins + ' min' + SpecMetrics.pluralize(durationInMins) + duration;
+        }
+        durationInHrs = Math.floor(durationInMins / 60);
+        durationInMins = durationInMins % 60;
+        if (durationInMins) {
+            duration = ' ' + durationInMins + ' min' + SpecMetrics.pluralize(durationInMins) + duration;
+        }
+        return durationInHrs + ' hour' + SpecMetrics.pluralize(durationInHrs) + duration;
+    }
+
+    private static pluralize(count) {
+        return count > 1 ? 's' : '';
+    }
 }
-
-module.exports = SpecMetrics;
