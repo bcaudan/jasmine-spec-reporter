67efdd7d36e08bd75c2c592fba7558669ca49741
Bastien Caudan <bastien.caudan@gmail.com>
2017-04-29 15:05:56 +0200

    Add greenkeeper yarn support for examples

--- greenkeeper-yarn.sh
+++ greenkeeper-yarn.sh
@@ -16,14 +16,23 @@ git checkout $TRAVIS_PULL_REQUEST_BRANCH
 # See if commit message includes "update"
 git log --name-status HEAD^..HEAD | grep "update" || exit 0
 
-echo "(Creat/updat)ing lockfile"
+echo "Updating lockfiles"
 yarn
+
+cd examples/node && yarn upgrade
+cd -
+
+cd examples/protractor && yarn upgrade
+cd -
+
+cd examples/typescript && yarn upgrade
+cd -
 
 echo "Commit and push yarn.lock"
 git config --global user.email "$PUSH_EMAIL"
 git config --global user.name "Travis CI"
 git config --global push.default simple
 
-git add yarn.lock
+git add yarn.lock examples/node/yarn.lock examples/protractor/yarn.lock examples/typescript/yarn.lock
 git commit -m "chore: update yarn.lock"
 git push
