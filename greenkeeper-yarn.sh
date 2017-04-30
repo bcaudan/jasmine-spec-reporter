b14886aaa7d99d476b097755879c92b33e202e34
Bastien Caudan <bastien.caudan@gmail.com>
2017-04-30 12:34:57 +0200

    greenkeeper yarn: check only direct dependencies for examples

--- greenkeeper-yarn.sh
+++ greenkeeper-yarn.sh
@@ -14,23 +14,23 @@ echo "Switching to branch $TRAVIS_PULL_REQUEST_BRANCH"
 git checkout $TRAVIS_PULL_REQUEST_BRANCH
 
 # See if commit message includes "update"
-git log --name-status HEAD^..HEAD | grep "update" || exit 0
+git log --name-status HEAD^..HEAD | grep "chore(package)" || exit 0
 
 echo "Updating lockfiles"
 yarn
 
 PACKAGE=`echo "$TRAVIS_PULL_REQUEST_BRANCH" | sed 's/[^-]*-\(.*\)/\1/' | sed 's/\(.*\)-[^-]*/\1/'`
 
-cd examples/node
-yarn list | grep $PACKAGE && yarn upgrade $PACKAGE
+cd examples/node && echo "Check examples/node"
+cat package.json | grep "\"$PACKAGE\":" && yarn upgrade $PACKAGE
 cd -
 
-cd examples/protractor
-yarn list | grep $PACKAGE && yarn upgrade $PACKAGE
+cd examples/protractor && echo "Check examples/protractor"
+cat package.json | grep "\"$PACKAGE\":" && yarn upgrade $PACKAGE
 cd -
 
-cd examples/typescript
-yarn list | grep $PACKAGE && yarn upgrade $PACKAGE
+cd examples/typescript && echo "Check examples/typescript"
+cat package.json | grep "\"$PACKAGE\":" && yarn upgrade $PACKAGE
 cd -
 
 echo "Commit and push yarn.lock"
@@ -39,5 +39,5 @@ git config --global user.name "Travis CI"
 git config --global push.default simple
 
 git add yarn.lock examples/node/yarn.lock examples/protractor/yarn.lock examples/typescript/yarn.lock
-git commit -m "chore: update yarn.lock"
+git commit -m "chore(yarn.lock): update yarn.lock"
 git push
