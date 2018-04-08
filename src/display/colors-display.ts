3ee8a40dca7ee299a1d597580e17c504f5ee849c
Bastien Caudan <bastien.caudan@gmail.com>
2018-04-08 10:36:28 +0200

    fix(package): remove @types/colors dependency

--- src/display/colors-display.ts
+++ src/display/colors-display.ts
@@ -1,9 +1,9 @@
-import colors = require("colors");
+import * as colors from "colors";
 import {Configuration} from "../configuration";
 
 export class ColorsDisplay {
     public static init(configuration: Configuration): void {
-        colors.enabled = configuration.colors.enabled;
+        (colors as any).enabled = configuration.colors.enabled;
         colors.setTheme({
             failed: configuration.colors.failed,
             pending: configuration.colors.pending,
