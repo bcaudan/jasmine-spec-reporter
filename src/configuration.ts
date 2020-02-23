7b1f82c5f294ff4f11a3a43da6675f6fb55796c6
Bastien Caudan <bastien.caudan@gmail.com>
2020-02-23 13:32:35 +0100

    update tslint

--- src/configuration.ts
+++ src/configuration.ts
@@ -120,7 +120,7 @@ export class Configuration {
      * list of display processor to customize output
      * see https://github.com/bcaudan/jasmine-spec-reporter/blob/master/docs/customize-output.md
      */
-    public customProcessors?: Array<typeof DisplayProcessor>;
+    public customProcessors?: (typeof DisplayProcessor)[];
     /**
      * options for custom processors
      */
